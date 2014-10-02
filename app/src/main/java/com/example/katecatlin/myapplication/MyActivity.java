66ade264c867ad16ab9f73107e250bbb541e88f9
Kate Catlin <kate.catlin@detroitlabs.com>
2014-10-01 20:53:29 -0400

    Adding text view optiongit add .

--- app/src/main/java/com/example/katecatlin/myapplication/MyActivity.java
+++ app/src/main/java/com/example/katecatlin/myapplication/MyActivity.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 
 public class MyActivity extends Activity implements View.OnClickListener {
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,16 @@ public class MyActivity extends Activity implements View.OnClickListener {
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
         mainEditText = (EditText) findViewById(R.id.main_edittext);
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
     }
 
 
@@ -50,6 +67,8 @@ public class MyActivity extends Activity implements View.OnClickListener {
     public void onClick(View view) {
         mainTextView.setText(mainEditText.getText().toString()
                 + " is learning Android development!");
-
+        // Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
     }
 }
